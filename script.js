273676ad9f45bea7196e9e832bac657bd9d53043
seawebtest <130911214+seawebtest@users.noreply.github.com>
2023-04-19 19:41:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function navBar() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
